47cf2387344398bd70635e5d2a738de44ab0ea51
FelixBragaM <fbragamenendez@udesa.edu.ar>
2024-06-17 19:39:17 -0300

    Home seccion 2

--- js/index.js
+++ js/index.js
@@ -23,7 +23,29 @@ fetch('https://fakestoreapi.com/products/category/jewelery')
     console.log("Error" + error);
 })
 
+let mensClothing = document.querySelector("#mensClothing");  
+fetch("https://fakestoreapi.com/products/category/men's clothing") 
+.then(function(response){
+    console.log(response);
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    for (let i = 0; i < 4; i++) {
+        mensClothing.innerHTML += `<article class="info">
+        <h3>${data[i].title}</h3>
+        <img class="foto" src="${data[i].image}" alt="">
+        <p>${data[i].description}</p>
+        <p>USD ${data[i].price}</p>
+        <a href="./producto.html?id=${data[i].id}"><button>Ver Mas</button></a>
+    </article>`
+        
+    }
+})
 
+.catch(function(error){
+    console.log("Error" + error);
+})
 
 
 
