87374803056b1084a8784de966e5cfff9151c2d9
Basavilbaso <fbasavilbaso@udesa.edu.ar>
2024-06-17 19:43:45 -0300

    Home seccion 3

--- js/index.js
+++ js/index.js
@@ -47,6 +47,30 @@ fetch("https://fakestoreapi.com/products/category/men's clothing")
     console.log("Error" + error);
 })
 
+let womensClothing = document.querySelector("#womensClothing");
+fetch("https://fakestoreapi.com/products/category/women's clothing")
+.then(function(response){
+    console.log(response);
+    return response.json();
+})
+.then(function(data){ 
+    console.log(data);
+    for (let i = 0; i < 6; i++) {          
+        womensClothing.innerHTML += `<article class="info">
+        <h3>${data[i].title}</h3>
+        <img class="foto" src="${data[i].image}" alt="">
+        <p>${data[i].description}</p>                      
+        <p>USD ${data[i].price}</p>  
+        <a href="./producto.html?id=${data[i].id}"><button>Ver Mas</button></a>
+    </article>`
+        
+    }
+})
+
+.catch(function(error){
+    console.log("Error" + error);
+})
+
 
 
 
