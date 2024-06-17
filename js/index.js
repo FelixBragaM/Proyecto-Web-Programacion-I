e13d854cc0afa9639934990ac6c67a4e1455b951
Basavilbaso <fbasavilbaso@udesa.edu.ar>
2024-06-17 19:27:17 -0300

    modificando

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-let jewelery = document.querySelector("#jewerly");
-fetch('https://fakestoreapi.com/products/category/jewelery')
-.then(function(response){
-   console.log(response);
-   return response.json();
-})
-.then(function(data){
-    console.log(data);
-    for (let i = 0; i < 4; i++) {
-        jewelery.innerHTML += `<article class="info">
-        <h3 class= "tituloProd">${data[i].title}</h3>
-        <img class="foto" src="${data[i].image}" alt="">
-        <p class= "description">${data[i].description}</p>
-        <p class="precioProd">USD ${data[i].price}</p>
-        <a href="./producto.html?id=${data[i].id}"><button>Ver Mas</button></a>
-    </article>`
-        
-    }
-    
-})
-
-.catch(function(error){
-    console.log("Error" + error);
-})
 
 
 
@@ -31,26 +7,3 @@ fetch('https://fakestoreapi.com/products/category/jewelery')
 
 
 
-let womensClothing = document.querySelector("#womensClothing");
-fetch("https://fakestoreapi.com/products/category/women's clothing")
-.then(function(response){
-    console.log(response);
-    return response.json();
-})
-.then(function(data){ 
-    console.log(data);
-    for (let i = 0; i < 6; i++) {          
-        womensClothing.innerHTML += `<article class="info">
-        <h3>${data[i].title}</h3>
-        <img class="foto" src="${data[i].image}" alt="">
-        <p>${data[i].description}</p>                      
-        <p>USD ${data[i].price}</p>  
-        <a href="./producto.html?id=${data[i].id}"><button>Ver Mas</button></a>
-    </article>`
-        
-    }
-})
-
-.catch(function(error){
-    console.log("Error" + error);
-})
