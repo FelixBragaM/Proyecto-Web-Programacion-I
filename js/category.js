0d3ce2b50d3c32f589924b8925ab1f57552456ee
FelixBragaM <fbragamenendez@udesa.edu.ar>
2024-06-18 11:12:46 -0300

    Desarollo del punto de categoria

---
+++ js/category.js
@@ -0,0 +1,28 @@
+let cat = document.querySelector(".ca");
+let queryString = location.search;
+let queryStringObject = new URLSearchParams(queryString);
+let categoria = queryStringObject.get("categoria");
+let url = "https://fakestoreapi.com/products/category/${categoria}";
+
+fetch(url)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    for (let i = 0; i < 4; i++) {
+        cat.innerHTML += `<article class="info">
+        <h3 class= "tituloProd">${data[i].title}</h3>
+        <img class="foto" src="${data[i].image}" alt="">
+        <p class= "description">${data[i].description}</p>
+        <p class="precioProd">USD ${data[i].price}</p>
+        <a href="./producto.html?id=${data[i].id}"><button>Ver Mas</button></a>
+    </article>`
+        
+    }
+    
+})
+
+.catch(function(error){
+    console.log("Error" + error);
+})
